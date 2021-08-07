bd334a579501545efee160f3e4b881c281eb908a
Sven Buettner <sbuettner@nosgroup.de>
2021-08-07 21:34:01 +0200

    Third solved test

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,10 @@
 // solution Test 2
 function fizzbuzz(input) {
+
+    if(input % 3 === 0) {
+        return 'Fizz';
+    }
+
     return input.toString();
 }
 
--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -5,26 +5,20 @@ const { fizzbuzz } = require("./fizzbuzz");
  */
 describe("test function fizzbuss", () => {
 
-    /**
-     * Test 1
-     * check if the fizzbuzz function is existing.
-     * red phase: fizzbuzz is not defined
-     * green phase: create and export the function fizzbuzz
-     */
     it("should can find the function fizzbuzz",() => {
         const result = fizzbuzz(1);
     });
 
-    /**
-     * Test 2
-     * check if the fizzbuzz function is returning the input value 1
-     * red phase: received: undefined
-     * green phase: received: 1
-     */
     it("should return '1' for 1",() => {
         const input = 1;
         const result = fizzbuzz(input);
         expect(result).toBe('1')
+    });
+
+    it("should return 'Fizz' for 3", () => {
+        let input = 3;
+        let result = fizzbuzz(input); 
+        expect(result).toBe('Fizz');
     });
 
 });
